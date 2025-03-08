96a2ef448f88f2babaa9bae0dd82ee6f23d2396f
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 01:22:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,3 @@ app.add_middleware(
 @app.get("/", response_class=JSONResponse)
 def read_root():
     return JSONResponse(content={"message": "🚀 FastAPI con SSL habilitado"}, media_type="application/json")
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        ssl_keyfile="/etc/letsencrypt/live/equalitech.xyz/privkey.pem",
-        ssl_certfile="/etc/letsencrypt/live/equalitech.xyz/fullchain.pem"
-    )
