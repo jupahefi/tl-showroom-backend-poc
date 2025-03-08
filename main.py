20401937b395bdf5b03799d04c2fcd715a23def9
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 00:44:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permitir todas las conexiones (en producción usa solo los dominios necesarios)
+    allow_origins=["https://tl-showroom.equalitech.xyz"],  # Reemplaza con el dominio de tu frontend
     allow_credentials=True,
-    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE, OPTIONS, etc.)
-    allow_headers=["*"],  # Permitir todos los headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
