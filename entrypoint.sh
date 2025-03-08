aa73403cc0834914973d4921107b24110229a986
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 01:28:58 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "🚀 Iniciando API con HTTPS..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000 \
-    --ssl-keyfile /etc/letsencrypt/live/equalitech.xyz/privkey.pem \
-    --ssl-certfile /etc/letsencrypt/live/equalitech.xyz/fullchain.pem
+ echo "🚀 Iniciando API..."
+ exec uvicorn main:app --host 0.0.0.0 --port 8000
