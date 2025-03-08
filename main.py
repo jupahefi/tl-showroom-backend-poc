243899f0218eb0de878c810d27fdc93506299e33
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 00:51:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tl-showroom.equalitech.xyz"],  # Reemplaza con el dominio de tu frontend
+    allow_origins=["https://tl-showroom.equalitech.xyz"],  # Reemplaza con el dominio correcto
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/", response_class=JSONResponse)
 def read_root():
-    return {"message": "🚀 FastAPI funcionando correctamente con CORS habilitado!"}
+    return JSONResponse(content={"message": "🚀 FastAPI funcionando correctamente con CORS habilitado!"}, media_type="application/json")
