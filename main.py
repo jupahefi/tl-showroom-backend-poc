aef45d130ea796e9f4ed65c4b23a856245c4bc70
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 01:09:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-"""app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tl-showroom.equalitech.xyz"],  # Asegura que coincida con tu frontend
+    allow_origins=["https://tl-showroom.equalitech.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)"""
+)
 
 @app.get("/", response_class=JSONResponse)
 def read_root():
-    return JSONResponse(content={"message": "🚀 FastAPI funcionando correctamente con CORS habilitado! y un mensaje porque asi reconoces que si llegue"}, media_type="application/json")
+    return JSONResponse(content={"message": "🚀 FastAPI con SSL habilitado"}, media_type="application/json")
 
 if __name__ == "__main__":
     uvicorn.run(
