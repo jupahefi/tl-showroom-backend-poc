297c3379a09ecc700a4bb8c7a4ffcb1dd6481c23
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-08 01:31:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://tl-showroom.equalitech.xyz:8000/"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/", response_class=JSONResponse)
 def read_root():
-    return JSONResponse(content={"message": "🚀 FastAPI con SSL habilitado"}, media_type="application/json")
+    return JSONResponse(content={"message": "🚀 FastAPI sin SSL habilitado"}, media_type="application/json")
